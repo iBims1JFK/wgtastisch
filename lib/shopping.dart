6ba35983261bead865327a314555c79f6305c4da
iBims1JFK <jonathan.kinzel@googlemail.com>
2020-10-19 13:03:16 +0200

    Auswertung funktioniert besser

--- lib/shopping.dart
+++ lib/shopping.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:tesseract_ocr/tesseract_ocr.dart';
-import 'Widgets/CartItemList.dart';
+import 'Widgets/CartList.dart';
 
 class ShoppingPage extends StatefulWidget {
   @override
@@ -19,10 +19,76 @@ class ShoppingPage extends StatefulWidget {
 class _ShoppingPageState extends State<ShoppingPage> {
   String _extractText = 'Nicht gefunden';
   List<String> CartItems = [];
+  List<String> product = [];
+  List<double> price = [];
+
   @override
   void initState() {
     super.initState();
     initPlatformState();
+  }
+
+  bool isNumeric(String s) {
+    if (s == null) {
+      return false;
+    }
+    return double.parse(s, (e) => null) != null;
+  }
+
+  void getRelevantInformation(List<String> fullStrings) {
+    List<List<String>> splitedStrings = [];
+    List<String> Product = [];
+    List<double> Price = [];
+    RegExp regExp = new RegExp(
+      r"(\d*)(,)(\d*)"
+    );
+
+    fullStrings.forEach((e) {
+      splitedStrings.add(e.split(" "));
+    });
+    for (int l = 0; l < splitedStrings.length; l++) {
+      bool foundProduct = false;
+      bool foundPrice = false;
+      List<String> elementsToDelete = [];
+      for (int i = 0; i < splitedStrings[l].length; i++) {
+        if (!foundProduct) {
+          if (isNumeric(splitedStrings[l][i])) {
+            elementsToDelete.add(splitedStrings[l][i]);
+          } else {
+            foundProduct = true;
+          }
+        }
+        if (!foundPrice) {
+          if (regExp.hasMatch(splitedStrings[l][i])) {
+            foundPrice = true;
+            // Price.add(double.parse(splitedStrings[l][i].replaceAll(",", ".")));
+            //nicht alles muss ein double sein
+            Price.add(double.parse(splitedStrings[l][i].replaceAll("/", "7").replaceAll(",", ".")));
+            elementsToDelete.add(splitedStrings[l][i]);
+          }
+        } else {
+          elementsToDelete.add(splitedStrings[l][i]);
+        }
+      }
+      if(foundPrice && foundProduct){
+        for (String s in elementsToDelete) {
+          splitedStrings[l].remove(s);
+        }
+      } else {
+        splitedStrings[l].removeWhere((element) => true);
+      }
+
+    }
+    for (int l = 0; l < splitedStrings.length; l++) {
+      if(splitedStrings[l].isNotEmpty){
+        Product.add(splitedStrings[l].toString().replaceAll(",", "").replaceAll("[", "").replaceAll("]", ""));
+      }
+    }
+    print(Product.length);
+    print(Price.length);
+
+    this.product = Product;
+    this.price = Price;
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
@@ -42,8 +108,6 @@ class _ShoppingPageState extends State<ShoppingPage> {
       );
       await File(imagePath).writeAsBytes(bytes);
       extractText = await TesseractOcr.extractText(imagePath, language: "deu");
-
-
     } on PlatformException {
       extractText = 'Failed to extract text';
     }
@@ -51,12 +115,10 @@ class _ShoppingPageState extends State<ShoppingPage> {
     // If the widget was removed from the tree while the asynchronous platform
     // message was in flight, we want to discard the reply rather than calling
     // setState to update our non-existent appearance.
+    // setState to update our non-existent appearance.
     if (!mounted) return;
     CartItems = extractText.split('\n');
-    List<List<String>> CartItemsMapped = [];
-    //CartItems.forEach((element) {
-    //  CartItemsMapped.add()
-    //});
+    getRelevantInformation(CartItems);
     setState(() {
       _extractText = extractText;
     });
@@ -67,9 +129,8 @@ class _ShoppingPageState extends State<ShoppingPage> {
     return Column(
       children: [
         Container(
-                    child: Text('Detected Text: $CartItems[0]\n'),
-
-         // child: CartItemList(),
+          // child: Text('Detected Text: $CartItems[0]\n'),
+          child: CartList(product, price),
         ),
         Image.asset('lib/bill.jpg'),
       ],
